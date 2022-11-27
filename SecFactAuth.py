a2de4c21adb832919484d82014fa71d3ea2296fb
Bhupesh Kumbhar <bhupesh.kumbhar@gmail.com>
2022-11-27 19:47:50 +0530

    Add Message Box

--- SecFactAuth.py
+++ SecFactAuth.py
@@ -1,11 +1,16 @@
 from requests.auth import HTTPBasicAuth
+from easygui import enterbox
+from easygui import msgbox
 import requests
+token = enterbox("Please enter Token")
 api_url = 'https://easy-authenticator.p.rapidapi.com/verify'
-api_qry = {"secretCode":"333K6O6QVP5SH7UR7JH7A6RGT3LZMWZB","token":"628235","name":"bhupesh.kumbhar"}
+api_qry = {"secretCode":"333K6O6QVP5SH7UR7JH7A6RGT3LZMWZB","token":token,"name":"bhupesh.kumbhar"}
 api_hdr = {"X-Rapidapi-Key":"472f53a83bmsh0945009f2fe0c7bp1d4da4jsn137f40e946b5","X-RapidAPI-Host": "easy-authenticator.p.rapidapi.com"}
 
 response = requests.request("POST",api_url,headers=api_hdr,params=api_qry)
 if response.json()["verify"]:
     print("Status : True")
+    msgbox("Login Successful","Message","Continue")
 else:
     print("Status : False")
+    msgbox("Login Failed","Message","Continue")
