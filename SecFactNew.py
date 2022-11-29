c3265c835fafeb487d7d2599900ad4341caf5110
Bhupesh Kumbhar <bhupesh.kumbhar@gmail.com>
2022-11-29 13:54:49 +0530

    Add Sandeep Kulkarni

--- SecFactNew.py
+++ SecFactNew.py
@@ -1,7 +1,7 @@
 from requests.auth import HTTPBasicAuth
 import requests
 api_url = 'https://easy-authenticator.p.rapidapi.com/newAuthKey?'
-api_qry = {"account":"Bhupesh.Kumbhar","issuer":"ZENCON"}
+api_qry = {"account":"Sandeep.Kulkarni","issuer":"ZENCON"}
 api_hdr = {"X-Rapidapi-Key":"472f53a83bmsh0945009f2fe0c7bp1d4da4jsn137f40e946b5","X-RapidAPI-Host": "easy-authenticator.p.rapidapi.com"}
 
 response = requests.request("POST",api_url,headers=api_hdr,params=api_qry)
